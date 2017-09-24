2c83b69cf6c55b1535edae3b4ed29909efbb802c
Alexander Olkhovskiy <AlexanderOlkhovskiy@gmail.com>
2017-09-24 23:06:19 +0300

    Initial version of the mod

---
+++ README.md
@@ -0,0 +1,5 @@
+### 0 A.D. Civ Choices Mod
+
+Adds the civ choices dialog allowing to select one of mutually exclusive technologies at the start of the match.
+
+Example technologies are included for Athenians: either Hoplite or Slinger Militia are unlocked in CC via the civ choices dialog.
---
+++ gui/session/civ_choices.js
@@ -0,0 +1,26 @@
+function initCivChoicesDialog()
+{
+	let currentPlayer = g_Players[Engine.GetPlayerID()];
+	let civChoices = g_CivData[currentPlayer.civ].CivChoices;
+	if (civChoices)
+	{
+		for (let i = 0; i < civChoices.length; ++i)
+		{
+			let civChoiceButton = Engine.GetGUIObjectByName("civChoice[" + i + "]");
+			civChoiceButton.caption = GetTechnologyData(civChoices[i]).name.generic;
+
+			let size = civChoiceButton.size;
+			size.top = 20 * i;
+			size.bottom = 20 * (i + 1);
+			civChoiceButton.size = size;
+
+			civChoiceButton.onPress = (function(tech) { return function() {
+				Engine.PostNetworkCommand({ "type": "civ-choice", "template": tech });
+				Engine.GetGUIObjectByName("civChoicesDialogPanel").hidden = true;
+			}})(civChoices[i]);
+
+			civChoiceButton.hidden = false;
+		}
+		Engine.GetGUIObjectByName("civChoicesDialogPanel").hidden = false;
+	}
+}

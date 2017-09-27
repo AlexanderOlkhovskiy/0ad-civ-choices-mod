4cee038f780994a91a1bf7d24fc0aa81952bf014
Alexander Olkhovskiy <AlexanderOlkhovskiy@gmail.com>
2017-09-27 20:58:50 +0300

    Fix replays and presumably observers

--- gui/session/civ_choices.js
+++ gui/session/civ_choices.js
@@ -1,26 +1,29 @@
 function initCivChoicesDialog()
 {
+	if (Engine.GetPlayerID() < 0)
+		return;
+
 	let currentPlayer = g_Players[Engine.GetPlayerID()];
 	let civChoices = g_CivData[currentPlayer.civ].CivChoices;
-	if (civChoices)
+	if (civChoices === undefined)
+		return;
+	
+	for (let i = 0; i < civChoices.length; ++i)
 	{
-		for (let i = 0; i < civChoices.length; ++i)
-		{
-			let civChoiceButton = Engine.GetGUIObjectByName("civChoice[" + i + "]");
-			civChoiceButton.caption = GetTechnologyData(civChoices[i]).name.generic;
+		let civChoiceButton = Engine.GetGUIObjectByName("civChoice[" + i + "]");
+		civChoiceButton.caption = GetTechnologyData(civChoices[i]).name.generic;
 
-			let size = civChoiceButton.size;
-			size.top = 20 * i;
-			size.bottom = 20 * (i + 1);
-			civChoiceButton.size = size;
+		let size = civChoiceButton.size;
+		size.top = 20 * i;
+		size.bottom = 20 * (i + 1);
+		civChoiceButton.size = size;
 
-			civChoiceButton.onPress = (function(tech) { return function() {
-				Engine.PostNetworkCommand({ "type": "civ-choice", "template": tech });
-				Engine.GetGUIObjectByName("civChoicesDialogPanel").hidden = true;
-			}})(civChoices[i]);
+		civChoiceButton.onPress = (function(tech) { return function() {
+			Engine.PostNetworkCommand({ "type": "civ-choice", "template": tech });
+			Engine.GetGUIObjectByName("civChoicesDialogPanel").hidden = true;
+		}})(civChoices[i]);
 
-			civChoiceButton.hidden = false;
-		}
-		Engine.GetGUIObjectByName("civChoicesDialogPanel").hidden = false;
+		civChoiceButton.hidden = false;
 	}
+	Engine.GetGUIObjectByName("civChoicesDialogPanel").hidden = false;
 }
